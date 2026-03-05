272f1cc87d6fce72a4fdc62e93c0349c9c43289d
unknown <khaledgaber494@gmail.com>
2026-03-05 16:49:48 +0200

    strts

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000/onlinestore",
+  baseURL:
+    "https://onlinestore-production-d7b2.up.railway.app/onlinestore" ||
+    "http://localhost:8000/onlinestore",
   headers: {
     "Content-Type": "application/json",
   },
