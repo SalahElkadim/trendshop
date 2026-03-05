c951d8a63e138a03d2750a38a3787f2cf5ef1fe8
unknown <khaledgaber494@gmail.com>
2026-03-05 16:59:14 +0200

    strts

--- src/api/axios.js
+++ src/api/axios.js
@@ -7,7 +7,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true, // ← هنا
+  withCredentials:false, // ← هنا
 });
 
 // ── Request Interceptor: ضيف الـ Token تلقائياً ──────────────
